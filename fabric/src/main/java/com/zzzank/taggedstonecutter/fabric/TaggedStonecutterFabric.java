26eb838237861940de81e2ee211e889f85cce5fc
ZZZank <3410764033@qq.com>
2024-03-26 11:23:01 +0800

    recipe fetching for fabric

--- fabric/src/main/java/com/zzzank/taggedstonecutter/fabric/TaggedStonecutterFabric.java
+++ fabric/src/main/java/com/zzzank/taggedstonecutter/fabric/TaggedStonecutterFabric.java
@@ -1,14 +1,19 @@
 package com.zzzank.taggedstonecutter.fabric;
 
+import com.zzzank.taggedstonecutter.recipe.DummyRecipeGenerater;
 import com.zzzank.taggedstonecutter.recipe.TagAddingRecipe;
-
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.core.Registry;
 
 public class TaggedStonecutterFabric implements ModInitializer {
+
     @Override
     public void onInitialize() {
         Registry.register(Registry.RECIPE_SERIALIZER, TagAddingRecipe.TYPE_ID, TagAddingRecipe.SERIALIZER);
         Registry.register(Registry.RECIPE_TYPE, TagAddingRecipe.TYPE_ID, TagAddingRecipe.TYPE);
+        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+            DummyRecipeGenerater.setServer(server);
+        });
     }
 }
