5485110bbff20d308065f78c5325940d0ef894a8
ZZZank <3410764033@qq.com>
2024-04-09 14:36:21 +0800

    clear server reference on server stop
    prevent resource leak

--- fabric/src/main/java/com/zzzank/taggedstonecutter/fabric/TaggedStonecutterFabric.java
+++ fabric/src/main/java/com/zzzank/taggedstonecutter/fabric/TaggedStonecutterFabric.java
@@ -13,8 +13,12 @@ public class TaggedStonecutterFabric implements ModInitializer {
     public void onInitialize() {
         Registry.register(Registry.RECIPE_SERIALIZER, TagAddingRecipeType.ID, TagAddingRecipe.SERIALIZER);
         Registry.register(Registry.RECIPE_TYPE, TagAddingRecipeType.ID, TagAddingRecipe.TYPE);
+
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
             DummyRecipeGenerater.setServer(server);
+        });
+        ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
+            DummyRecipeGenerater.setServer(null);
         });
         ServerLifecycleEvents.START_DATA_PACK_RELOAD.register((server, res) -> {
             DummyRecipeGenerater.clearCache();
--- forge/src/main/java/com/zzzank/taggedstonecutter/forge/TaggedStonecutterForge.java
+++ forge/src/main/java/com/zzzank/taggedstonecutter/forge/TaggedStonecutterForge.java
@@ -10,6 +10,7 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
+import net.minecraftforge.fml.event.server.FMLServerStoppedEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -28,6 +29,9 @@ public class TaggedStonecutterForge {
 
         MinecraftForge.EVENT_BUS.addListener((FMLServerAboutToStartEvent event) -> {
             DummyRecipeGenerater.setServer(event.getServer());
+        });
+        MinecraftForge.EVENT_BUS.addListener((FMLServerStoppedEvent event) -> {
+            DummyRecipeGenerater.setServer(null);
         });
         MinecraftForge.EVENT_BUS.addListener((AddReloadListenerEvent event) -> {
             DummyRecipeGenerater.clearCache();
