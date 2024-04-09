c2ac5851472d9282810afc2b41062b2a71b70256
ZZZank <3410764033@qq.com>
2024-04-09 11:59:19 +0800

    implement shift-click
    yeaaaaaaaaaaah

--- common/src/main/java/com/zzzank/taggedstonecutter/mixin/StonecutterMenuMixin.java
+++ common/src/main/java/com/zzzank/taggedstonecutter/mixin/StonecutterMenuMixin.java
@@ -3,7 +3,10 @@ package com.zzzank.taggedstonecutter.mixin;
 import com.zzzank.taggedstonecutter.recipe.DummyRecipeGenerater;
 import java.util.List;
 import net.minecraft.world.Container;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.DataSlot;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.inventory.StonecutterMenu;
 import net.minecraft.world.item.ItemStack;
@@ -14,9 +17,14 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(StonecutterMenu.class)
-public abstract class StonecutterMenuMixin {
+public abstract class StonecutterMenuMixin extends AbstractContainerMenu {
+
+    private StonecutterMenuMixin(MenuType<?> menuType, int containerId) {
+        super(menuType, containerId);
+    }
 
     @Shadow
     private List<StonecutterRecipe> recipes;
@@ -42,5 +50,29 @@ public abstract class StonecutterMenuMixin {
         this.resultSlot.set(ItemStack.EMPTY);
         this.recipes = dummyRecipes;
         ci.cancel();
+    }
+
+    /**
+     * @see net.minecraft.world.inventory.StonecutterMenu#quickMoveStack(Player, int)
+     */
+    @Inject(
+        method = "quickMoveStack",
+        at = @At(value = "INVOKE", target = "Ljava/util/Optional;isPresent()Z"),
+        cancellable = true
+    )
+    public void onQuickMoveStack(Player player, int index, CallbackInfoReturnable<ItemStack> ci) {
+        if (index == 0 || index == 1) {
+            return;
+        }
+        Slot slot = this.slots.get(index);
+        if (slot == null || !slot.hasItem()) {
+            return;
+        }
+        if (
+            DummyRecipeGenerater.tryMatch(slot.getItem().getItem()) != null &&
+            !this.moveItemStackTo(slot.getItem(), 0, 1, false)
+        ) {
+            ci.setReturnValue(ItemStack.EMPTY);
+        }
     }
 }
