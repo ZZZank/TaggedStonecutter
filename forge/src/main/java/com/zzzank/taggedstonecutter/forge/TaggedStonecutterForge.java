7b3303d8182fa44b577887f0ade1b5ec2373821b
ZZZank <3410764033@qq.com>
2024-04-09 14:59:30 +0800

    remove arch api usage

--- forge/src/main/java/com/zzzank/taggedstonecutter/forge/TaggedStonecutterForge.java
+++ forge/src/main/java/com/zzzank/taggedstonecutter/forge/TaggedStonecutterForge.java
@@ -4,26 +4,18 @@ import com.zzzank.taggedstonecutter.TaggedStonecutter;
 import com.zzzank.taggedstonecutter.recipe.DummyRecipeGenerater;
 import com.zzzank.taggedstonecutter.recipe.TagAddingRecipe;
 import com.zzzank.taggedstonecutter.recipe.TagAddingRecipe.TagAddingRecipeType;
-import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraft.world.item.crafting.RecipeType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.event.server.FMLServerStoppedEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod(TaggedStonecutter.MOD_ID)
 public class TaggedStonecutterForge {
 
     public TaggedStonecutterForge() {
-        // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(
-            TaggedStonecutter.MOD_ID,
-            FMLJavaModLoadingContext.get().getModEventBus()
-        );
-
         ForgeRegistries.RECIPE_SERIALIZERS.register(TagAddingRecipe.SERIALIZER);
         RecipeType.register(TagAddingRecipeType.ID.toString());
 
